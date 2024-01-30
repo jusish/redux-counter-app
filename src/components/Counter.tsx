d962ad261a539a54f68c25c22ffe2f04cebbed3b
jusish <ishimwejustin67@gmail.com>
2024-01-30 12:14:17 +0200

    New changes

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,11 +1,6 @@
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../state/store";
-import {
-  decrement,
-  increment,
-  incrementByAmount,
-  incrementAsync,
-} from "../state/counter/counterSlice";
+import { decrement, incrementAsync } from "../state/counter/counterSlice";
 
 const Counter = () => {
   const count = useSelector((state: RootState) => state.counter.value);
@@ -14,7 +9,7 @@ const Counter = () => {
   return (
     <div>
       <h2>{count}</h2>
-      <div>
+      <div className="gap-5 ">
         <button onClick={() => dispatch(incrementAsync(10))}>Increment</button>
         <button onClick={() => dispatch(decrement())}>Decrement</button>
       </div>
